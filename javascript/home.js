b0897905b6fbabc6e2e8cc94055cd8a6634fdc40
Nuchi <nadinajauch@gmail.com>
2022-05-22 11:52:40 -0300

    Acomodé carpetas

---
+++ javascript/home.js
@@ -0,0 +1,16 @@
+function toggleUserMenu() {
+  console.log("TEST");
+  $("#divDesplegable").show;
+  //   $(".cd-main-content").toggleClass("user-is-visible");
+  //   $(".cd-main-header").toggleClass("user-is-visible");
+  //   $(".header-user-menu").toggleClass("is-visible");
+  //   //close search
+  //   $(".cd-search").removeClass("is-visible");
+  //   $(".cd-search-trigger").removeClass("search-is-visible");
+  //   $(".cd-overlay").removeClass("search-is-visible is-visible");
+  //   if ($(".user-menu-overlay").hasClass("is-visible")) {
+  //     $(".user-menu-overlay").removeClass("is-visible");
+  //   } else {
+  //     $(".user-menu-overlay").addClass("is-visible");
+  //   }
+}
