29cf5785dc07be93dd45d21dc6e5b68b58220ebf
Nuchi <nadinajauch@gmail.com>
2022-06-14 15:46:49 -0300

    Estructurado de home

--- javascript/menu.js
+++ javascript/menu.js
@@ -12,6 +12,10 @@ function desplegarHamburguesa() {
 
 function desplegarSubmenu(desplegable) {
   console.log(desplegable);
+  let submenu1 = document.getElementById("submenu1");
+  let submenu2 = document.getElementById("submenu2");
+  let submenu3 = document.getElementById("submenu3");
+
   function toggle(desplegable) {
     if (desplegable.classList.contains("submenu-active")) {
       desplegable.classList.remove("submenu-active");
@@ -22,16 +26,24 @@ function desplegarSubmenu(desplegable) {
       desplegable.classList.add("slide-in-top");
     }
   }
+  function cierreDemasDesplegables(d1, d2) {
+    d1.classList.remove("submenu-active");
+    d1.classList.add("submenu-inactive");
+    d2.classList.remove("submenu-active");
+    d2.classList.add("submenu-inactive");
+  }
 
   if (desplegable == "submenu1") {
-    let desplegable = document.getElementById("submenu1");
+    let desplegable = submenu1;
     toggle(desplegable);
+    cierreDemasDesplegables(submenu2, submenu3);
   } else if (desplegable == "submenu2") {
     let desplegable = document.getElementById("submenu2");
     toggle(desplegable);
+    cierreDemasDesplegables(submenu1, submenu3);
   } else if (desplegable == "submenu3") {
     let desplegable = document.getElementById("submenu3");
-    console.log(desplegable);
     toggle(desplegable);
+    cierreDemasDesplegables(submenu1, submenu2);
   }
 }
