bbd6efab559abf9f6963df74e188bcd047722392
Nuchi <nadinajauch@gmail.com>
2022-06-29 22:11:47 -0300

    mejora

--- javascript/movimientos.js
+++ javascript/movimientos.js
@@ -15,34 +15,54 @@ if (movimientos.length == 0) {
   contenedor.append(div);
 }
 
+function generarNumeroRandom() {
+  return Math.random() * (1000 - 100) + 100;
+}
+
 function agregarMov() {
   // fixAltura();
   if (movimientos.length == 8) {
     document.getElementById("Movimientos").style.height =
       document.getElementById("Movimientos").clientHeight;
   }
-  //div contenedor donde se pushean todos los movimientos
-  let contenedor = document.getElementById("Movimientos");
-  //creo div y le asigno clase
-  let div = document.createElement("div");
-  div.className = "divisor-movimientos";
-  div.setAttribute("onclick", "mostrar('overlay')");
-  //creo elementos que voy a asignar más adelante
-  let ul = document.createElement("ul");
-  let liFecha = document.createElement("li");
-  let liTexto = document.createElement("li");
-  let liMetodoPago = document.createElement("li");
-
+  //creacion de nuevo objeto
   let objetoMovimiento = new Object();
+  //creo un numero random para asignarle un numero de operacion al objeto
+  function nOperacion(lista) {
+    if (lista.length > 0) {
+      for (x in lista) {
+        let nRandom = generarNumeroRandom();
+        if (x.nOperacion != nRandom) {
+          return nRandom;
+        }
+      }
+    } else return generarNumeroRandom();
+  }
 
+  //asigno datos hardcodeados a cada propiedad del objeto
   objetoMovimiento.tipoDeMovimiento = prompt("TIPO DE MOVIMIENTO SE REALIZÓ");
   objetoMovimiento.gasto = prompt("CUANTO GASTO");
   objetoMovimiento.metodo = prompt("Ingrese con qué pagó");
   objetoMovimiento.destino = prompt("DONDE O A QUIEN SE LE PAGÓ");
   objetoMovimiento.fecha = new Date().toLocaleString();
-
+  objetoMovimiento.nOperacion = nOperacion(movimientos);
   //pusheo mi objeto con todas sus propiedades recien asignadas
   movimientos.push(objetoMovimiento);
+  console.log(objetoMovimiento.nOperacion);
+  //div contenedor donde se pushean todos los movimientos
+  let contenedor = document.getElementById("Movimientos");
+  //creo div y le asigno clase
+  let div = document.createElement("div");
+  div.className = "divisor-movimientos";
+  div.setAttribute(
+    "onclick",
+    "mostrar('overlay');datosDelOverlay(" + objetoMovimiento.nOperacion + ")"
+  );
+  //creo elementos que voy a asignar más adelante
+  let ul = document.createElement("ul");
+  let liFecha = document.createElement("li");
+  let liTexto = document.createElement("li");
+  let liMetodoPago = document.createElement("li");
 
   liTexto.textContent =
     objetoMovimiento.tipoDeMovimiento +
@@ -98,4 +118,14 @@ function mostrar(id) {
   elemento.style.visibility = "visible";
 }
 
-function creacionOverlay() {}
+let overlay = document.getElementById("overlay");
+function datosDelOverlay(nOperacion) {
+  titulo = document.getElementById("tituloModal");
+  titulo.textContent = "n° de operación: " + nOperacion;
+  console.log(nOperacion);
+  // let contenido = document.createElement("p");
+
+  // contenido.textContent = objeto;
+
+  // document.getElementById("overlay-body").append(contenido);
+}
